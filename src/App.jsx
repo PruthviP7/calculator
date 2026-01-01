4af2f4b3402ebc01928cd70f1ae5f91a686c36a8
Pruthvi Arjunsingh Pardeshi <pruthvipardeshi.07@gmail.com>
2026-01-01 23:48:25 +0530

    Added the mapper for the calculator keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './app.css';
 
+const keyArr = ['AC', '%', '←', '➗', '7', '8', '9', '✕', '4', '5', '6', '-', '1', '2', '3', '+', '00', '0', '.', '='];
+
 function App() {
   return (
       <div className='main-container'>
@@ -8,19 +10,9 @@ function App() {
             <input disabled/>
           </div>
           <div id='button-container'>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>+</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div>-</div>
-            <div>7</div>
-            <div>8</div>
-            <div>9</div>
-            <div>*</div>
-            <div>0</div>
+           {keyArr.map((item, index) => {
+            return <div key={index}>{item}</div>
+           })}
           </div>
         </div>
       </div>   
